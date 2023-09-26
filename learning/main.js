281fa2d1a1e3c5e5802ed8cbc6c9cb07e7a9afdb
Boo-j <146093908+Boo-j@users.noreply.github.com>
2023-09-26 17:45:48 +0200

    code learning
    On a vue la base de la méthode logique et les conditions en apprenant comment user des logiciels.

---
+++ learning/main.js
@@ -0,0 +1,8 @@
+alert("BONSOIR !!! Je suis Bob Lennon, HAHA")
+
+console.log("Membre de Lenniona, AVANCE!")
+
+var cardname = "achdantafass"
+
+if(cardname == TRUE) {cardname = cardname + "valide"} else {cardname = cardname + "invalide"}
+
